7ad16b1cc8e0235d1921a3fa6c93e2f39ffc1463
sneha <snehanikule93@gmail.com>
2022-12-17 04:51:46 +0530

    counter

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{useState} from 'react';
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div className="App">
+     hello
+     <h2>{count}</h2>
+     <button onClick={()=>setCount(count+1)}>inc</button>
+    </div>
+  );
+}
+
+export default App;
